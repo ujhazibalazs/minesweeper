f53f0f545e2df45d51ac4a1e4ecf249948e38d29
Viktor Takács <takiviko@gmail.com>
2020-10-15 16:02:27 +0200

    Added Methods class, added initialize() method to the Field class.

--- src/main/java/Logic/Field.java
+++ src/main/java/Logic/Field.java
@@ -1,4 +1,28 @@
 package Logic;
 
 public class Field {
+
+    public Field(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.gameField = initialize();
+
+    }
+
+    private int width;
+    private int height;
+
+    private int numberOfMines;
+
+    Cell[][] gameField = new Cell[width][height];
+
+    private Cell[][] initialize() {
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField[0].length; j++) {
+                gameField[i][j] = new Cell();
+            }
+        }
+        return gameField;
+    }
+
 }
